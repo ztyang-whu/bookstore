422cf221bb0aaf76f6d6e416bacff4e149639f45
ztyang-whu <2212349804@qq.com>
2019-06-23 09:21:23 +0800

    update Index.java

--- online_bookstore/src/servlet/Index.java
+++ online_bookstore/src/servlet/Index.java
@@ -1,14 +1,24 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.MapHandler;
 
 /**
  * Servlet implementation class Index
+ * Author: yzt
+ * Description: 用来测试DBCP+DBUtils简化servlet访问数据库
+ * Date: 2019/6/23 
  */
 @WebServlet("/index")
 public class Index extends HttpServlet {
@@ -27,7 +37,18 @@ public class Index extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			DataSource ds=(DataSource) getServletContext().getAttribute("dataSource");
+			Connection conn=ds.getConnection();
+			String sql="SELECT * FROM users";
+			QueryRunner queryRunner=new QueryRunner();
+			Map<String, Object> result=queryRunner.query(conn, sql, new MapHandler());
+			System.out.println(result);
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
 	}
 
 	/**
